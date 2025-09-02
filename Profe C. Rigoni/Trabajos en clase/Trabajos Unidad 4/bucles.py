79e39f33d9de11cfb6a4c16af87959eaf45ae552
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-01 21:02:41 -0300

    Create bucles.py

---
+++ Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 4/bucles.py
@@ -0,0 +1,27 @@
+#TP bucles 
+
+
+abecedario="abcdefghijklmnopqrstuvxyz"
+mensaje = input("Ingrese su mensaje: ")
+corrimiento=int(input("Ingrese el corrimiento deseado: "))
+cifrado=""
+cant=5
+
+
+for letra_mensaje in mensaje:
+    if letra_mensaje in abecedario:
+        for i in range(len(abecedario)):
+          if letra_mensaje == abecedario[i]:
+              nueva_pos=(i + corrimiento)%26
+              nueva_letra= abecedario[nueva_pos]
+              cifrado=cifrado+nueva_letra
+
+print(cifrado)
+
+
+
+
+
+
+
+
