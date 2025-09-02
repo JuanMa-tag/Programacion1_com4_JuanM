97adc327f77adf3ff1750b337afbf5725e3419bb
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-01 21:57:33 -0300

    Update bucles.py

--- Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 4/bucles.py
+++ Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 4/bucles.py
@@ -1,17 +1,20 @@
-letras_abc="abcdefghijklmnopqrstuvxyz"
+letras_abc="abcdefghijklmnopqrstuvwxyz"
 cant=5
 for cant in range(5):
     print('-------------------------')
     print('OFICIAL', 1+cant)
+    cifrado=""
     mensaje = input("Ingrese su mensaje: ")
     corrimiento=int(input("Ingrese el corrimiento deseado: "))
     for letra_mensaje in mensaje:
-         if letra_mensaje in letras_abc:
-              for i in range(len(letras_abc)):
-                  if letra_mensaje == letras_abc[i]:
-                      nueva_pos=(i + corrimiento)%26
-                      nueva_letra= letras_abc[nueva_pos]
-                      cifrado=cifrado+nueva_letra
+        if letra_mensaje in letras_abc:
+            for i in range(len(letras_abc)):
+                if letra_mensaje == letras_abc[i]:
+                    nueva_pos=(i + corrimiento)%len(letras_abc)
+                    nueva_letra= letras_abc[nueva_pos]
+                    cifrado=cifrado+nueva_letra
+        else:
+            cifrado += letra_mensaje
     print(cifrado)
 
 
