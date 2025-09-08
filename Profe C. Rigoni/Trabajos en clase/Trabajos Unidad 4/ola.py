ce46280d27e27916ab2c117de801909d06c3554a
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-08 14:25:55 -0300

    Update ola.py

--- Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 4/ola.py
+++ Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 4/ola.py
@@ -1,10 +1,45 @@
 
-hemisferio= input('Ingrese el hemisferio donde se encuentra (norte/sur): ')
-mes_del_año= input('Ingrese un mes del año : ')
+hemisferio= input('Ingrese el hemisferio donde se encuentra (norte/sur): ').lower()
+mes_del_año= input('Ingrese un mes del año : ').lower()
 dia=int(input('ingrese un dia: '))
 
 # HEMISFERIO SUR 
 if hemisferio == 'sur':
-    if 
+    ## Primera Estación: (VERANO)
+    if (mes_del_año == 'diciembre') or (mes_del_año == 'enero') or (mes_del_año == 'febrero') or (mes_del_año == 'marzo'):
+        if mes_del_año == 'diciembre':
+            if dia >= 21 and dia <=31:
+                print('¡¡VERANO!!')
+        elif mes_del_año =='marzo':
+            if dia <=20:
+                print('¡¡VERANO!!')                   
+        else: 
+             if dia >= 1 and dia <31:
+                 print('¡¡VERANO!!')
+    ## Segunda Estacion: (OTOÑO)            
+    elif (mes_del_año == 'marzo') or (mes_del_año == 'abril') or (mes_del_año == 'mayo') or (mes_del_año == 'junio'):
+        if mes_del_año == 'marzo':
+            if dia >= 21 and dia <= 31:
+                print('¡¡OTOÑO!!')
+        elif mes_del_año == 'junio':
+            if dia <= 20 :
+                print('¡¡OTOÑO!!')
+                
+
+
+
+
+
+    
+
+
+
+            
+ 
+
+
+
+
+
 
     
