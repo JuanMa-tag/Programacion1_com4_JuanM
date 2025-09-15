00a36627bd2d2fc98dbca1ce15addf51b2c2a935
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-15 20:14:48 -0300

    .

---
+++ Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 5/ola.py
@@ -0,0 +1,82 @@
+# print('hola')
+# verdaded= True
+# opcion=0
+# while verdaded == True:
+#     print('como estas')
+#     opcion=int(input('ingresa un numero'))
+#     if opcion ==1:
+#         break
+#     else:
+#         print('bien y vos ')
+#         verdaded=False
+
+
+
+
+# cantidad_numeros= 4
+# max_num=0
+# for cont in range(cantidad_numeros):
+#     print('ingresa un numero: ', (cont+1))
+#     num=int(input())
+#     if num > max_num:
+#         max_num=num
+
+# print('El número maximo es: ',max_num)        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
