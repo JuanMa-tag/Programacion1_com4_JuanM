c031c7b7c65964db66528a30a5f8671cf66fdf97
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-16 17:24:10 -0300

    Update ola.py

---
+++ Trabajos Prácticos Campus/ola.py
@@ -0,0 +1,8 @@
+# 3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+# dados por el usuario, excluyendo esos dos valores.
+
+num1=int(input('Ingresa el primer número: '))
+num2=int(input('Ingresa el segundo número: '))
+
+for i in range(num1+1,num2,1)
+    
