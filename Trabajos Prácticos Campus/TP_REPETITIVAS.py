c334ed01d482e305d6684afc9bfce9d1dcca2069
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-16 17:20:14 -0300

    punto 2

--- Trabajos Prácticos Campus/TP_REPETITIVAS.py
+++ Trabajos Prácticos Campus/TP_REPETITIVAS.py
@@ -4,3 +4,13 @@
 # (incluyendo ambos extremos), en orden creciente, mostrando un número por línea
 for i in range(1,101,1):
     print(i)
+
+# 2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+# dígitos que contiene.
+
+num_digitos=int(input('ingresa un numero para saber sus digitos: '))
+
+digitos= len(str(num_digitos))
+
+print(f'Su número contiene {digitos} digitos')
+
