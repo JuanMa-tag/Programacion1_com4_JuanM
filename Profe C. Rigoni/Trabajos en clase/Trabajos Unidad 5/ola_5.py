9beb37c2eb460506fbf5544898697256568dd855
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-17 22:17:49 -0300

    Update ola_5.py

---
+++ Profe C. Rigoni/Trabajos en clase/Trabajos Unidad 5/ola_5.py
@@ -0,0 +1,12 @@
+# 3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
+# pantalla.
+
+lista_vacia=[]
+for i in range(3):
+    palabras=input(f'Ingresa la palabra Nro {i+1}: ')
+    lista_vacia.append(palabras)
+
+print(lista_vacia)
+
+
+
