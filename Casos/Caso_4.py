d98dc8ecbe2feb26bd899e68676ddc2fff1ce0f9
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-20 19:50:05 -0300

    Update Caso_4.py

---
+++ Casos/Caso_4.py
@@ -0,0 +1,36 @@
+clases=[]
+cupos=[]
+while True:
+    print('----MENU DE OPCIONES----')
+    print("""
+        1.Ingresar lista de clases
+        2.Ingresar cupos por clase
+        3.Mostrar clases con cupo
+        4.Consultar cupo de una clase
+        5.Listar clases sin cupo
+        6.Agregar clase
+        7.Ver sin cupo
+        8.Actualizar cupos (inscribir/baja)
+        9.Ver todas clases
+        10.Salir
+    """)
+    opcion=input('Ingresa la opcion deseada: ')
+    if opcion=='1':
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
