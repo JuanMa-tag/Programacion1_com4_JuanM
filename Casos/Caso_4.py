6e5ac7cc1dbab8528432e290e83cbb279d76cc1b
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-09-20 20:53:00 -0300

    Update Caso_4.py

--- Casos/Caso_4.py
+++ Casos/Caso_4.py
@@ -16,8 +16,40 @@ while True:
     """)
     opcion=input('Ingresa la opcion deseada: ')
     if opcion=='1':
-        
-
+        cant=int(input('Ingresa la cantidaad de clases que desea ingresar: '))
+        for i in range(cant):
+            clase_ingresada=input('Ingresa la clase: ')
+            clases.append(clase_ingresada)
+    elif opcion=='2':
+        for i in range(len(clases)):
+            print('Ingresa el cupo para cada clase')
+            cupos_ingresado=int(input('"'+ str(clases[i])+'": '))
+            cupos.append(cupos_ingresado)
+    elif opcion=='3':
+        for i in range(len(clases)):
+            print('Clase "'+ clases[i]+ '": '+ str(cupos[i])+ ' cupos')
+    elif opcion=='4':
+        buscar_clase=input('Ingresa la clase que desea consultar: ')
+        for i in range(len(clases)):
+            if buscar_clase == clases[i]:
+                print('"',clases[i],'"')
+                print(cupos[i],' cupos disponibles')
+    elif opcion=='5':
+        for i in range(len(clases)):
+            if cupos[i]==0:
+                print('La clase ',clases[i], ' no tiene cupos disponibles')
+    elif opcion=='6':
+        nueva_clase=input('Ingress la nueva clase: ')
+        nuevo_cupos=int(input('Igresa la cantidad de cupos disponibles: '))
+        clases.append(nueva_clase)
+        cupos.append(nuevo_cupos)
+    elif opcion =='8':
+        alumno=int(input('Ingresa  1. para inscribir un alumno/  2. para  dar de baja a un alumno: '))
+        clase_del_alumno=input('Ingresa la clase que quieras actualizar: ')
+        for i in range(len(clases)):
+            for i in range(len(clases)):
+                if clase_del_alumno == clases[i]:
+                    
 
 
 
