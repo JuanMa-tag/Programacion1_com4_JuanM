f5e0a020bf88e7d33c9291e9272dab03562653ff
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-10-14 18:16:50 -0300

    Update TP_DATOS_COMPLEJOS.py

--- Trabajos Prácticos Campus/TP_DATOS_COMPLEJOS.py
+++ Trabajos Prácticos Campus/TP_DATOS_COMPLEJOS.py
@@ -3,20 +3,62 @@
 # Naranja = 1200
 # Manzana = 1500
 # Pera = 2300
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+#antes
+print('Antes')
+print(precios_frutas, '\n')
+
+#se agregan mas frutas
+precios_frutas ['Naranja']=1200 
+precios_frutas ['manzana']=1500
+precios_frutas['pera']=2300
+
+#despues
+print('Despues')
+print(precios_frutas)
+
 
 # 2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 # desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
 # Banana = 1330
 # Manzana = 1700
 # Melón = 2800
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+#antes
+print('Antes')
+print(precios_frutas, '\n')
+
+#se agregan mas frutas
+precios_frutas ['Naranja']=1200 
+precios_frutas ['Manzana']=1500
+precios_frutas['Pera']=2300
+
+#despues
+print('Despues')
+print(precios_frutas, '\n')
+
+#Se actualizan los precios
+precios_frutas['Banana']=1330
+precios_frutas['Manzana']=1700
+precios_frutas['Melón']=2800
+print('Precios actualizados')
+print(precios_frutas)
+
 
 # 3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 # desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los
 # precios.
+precios_frutas={'Banana': 1330, 'Ananá': 2500, 'Melón': 2800, 'Uva': 1450, 'Naranja': 1200, 'Manzana': 1700, 'Pera': 2300}
+#Se guardan en una lista las frutas sin precios usando la funcion .keys()
+lista_de_frutas=precios_frutas.keys()
+print(lista_de_frutas)
+
 
 # 4) Escribí un programa que permita almacenar y consultar números telefónicos.
 #  Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
 #  Luego, pedí un nombre y mostrale el número asociado, si existe.
+
+
 
 # 5) Solicita al usuario una frase e imprime:
 #  Las palabras únicas (usando un set).
