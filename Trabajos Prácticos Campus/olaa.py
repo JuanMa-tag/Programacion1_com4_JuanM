d39b04c391116cfef19240fd09e42de324ba2905
JuanMa <carrillojuanmanuel2007@gmail.com>
2025-10-14 21:36:33 -0300

    Update olaa.py

--- Trabajos Prácticos Campus/olaa.py
+++ Trabajos Prácticos Campus/olaa.py
@@ -1,12 +1,11 @@
-# 4) Escribí un programa que permita almacenar y consultar números telefónicos.
-#  Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
-#  Luego, pedí un nombre y mostrale el número asociado, si existe.
-def nombres_claves():
-    pass
-
-
-
-
+# 5) Solicita al usuario una frase e imprime:
+#  Las palabras únicas (usando un set).
+#  Un diccionario con la cantidad de veces que aparece cada palabra.
+frase=input('ingrese una frase: ')
+palabras = frase.lower().split()
+palabras_unicas = set(palabras)
+print("\n Palabras Únicas")
+print(palabras_unicas)
 
 
 
